689e80521871fab0cb343edb1f8dad490b24647e
LeonardoRamos <leoSLRamos@gmail.com>
2014-05-20 16:02:20 -0300

    Update Usuario.java
    formatação

--- ES1Project/src/main/java/com/sistema/revistas/domain/Usuario.java
+++ ES1Project/src/main/java/com/sistema/revistas/domain/Usuario.java
@@ -68,26 +68,26 @@ public class Usuario {
 	
 	private String tornaSenhaSegura(String senhaParaHash) {
 		String salt = "sistemarevista"; 
-        String senhaGerada = null;
-        
-        try {
-            MessageDigest md = MessageDigest.getInstance("SHA-512");
-            md.update(salt.getBytes());
-            
-            byte[] bytes = md.digest(senhaParaHash.getBytes());
-            StringBuilder sb = new StringBuilder();
-            
-            for(int i = 0; i < bytes.length; i++) {
-                sb.append(Integer.toString((bytes[i] & 0xff) + 0x100, 16).substring(1));
-            }
-            
-            senhaGerada = sb.toString();
-        } catch (NoSuchAlgorithmException e) {
-            e.printStackTrace();
-        }
-        
-        return senhaGerada;
-    }
+        	String senhaGerada = null;
+	        
+	        try {
+	            MessageDigest md = MessageDigest.getInstance("SHA-512");
+	            md.update(salt.getBytes());
+	            
+	            byte[] bytes = md.digest(senhaParaHash.getBytes());
+	            StringBuilder sb = new StringBuilder();
+	            
+	            for(int i = 0; i < bytes.length; i++) {
+	                sb.append(Integer.toString((bytes[i] & 0xff) + 0x100, 16).substring(1));
+	            }
+	            
+	            senhaGerada = sb.toString();
+	        } catch (NoSuchAlgorithmException e) {
+	            e.printStackTrace();
+	        }
+	        
+		return senhaGerada;
+	}
 
 
 	public Long getId() {
